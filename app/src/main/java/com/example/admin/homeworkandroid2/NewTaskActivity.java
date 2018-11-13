640c6389a0292be632b3e9c9b768a986176e246c
borserb <borserb@gmail.com>
2018-11-13 21:04:18 +0200

    HomeWork 13.11 Spannable

--- app/src/main/java/com/example/admin/homeworkandroid2/NewTaskActivity.java
+++ app/src/main/java/com/example/admin/homeworkandroid2/NewTaskActivity.java
@@ -1,13 +1,24 @@
 package com.example.admin.homeworkandroid2;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Size;
 import android.view.View;
+import android.view.ViewDebug;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class NewTaskActivity extends AppCompatActivity {
@@ -20,6 +31,13 @@ public class NewTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        Spannable text = new SpannableString(getString(R.string.priority));
+        text.setSpan(new ForegroundColorSpan(getColor(R.color.redPoint)), 0, 1,  Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text.setSpan(new ForegroundColorSpan(getColor(R.color.black_38)), 2, getString(R.string.priority).length(),  Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text.setSpan(new StyleSpan(Typeface.BOLD), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        TextView textView = (TextView) findViewById(R.id.textViewPriority);
+        textView.setText(text);
 
         etTaskName=findViewById(R.id.etTaskName);
         ibNewItem=findViewById(R.id.ibNewItem);
